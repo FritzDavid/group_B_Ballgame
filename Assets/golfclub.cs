fbc9e85bfd48e76f5ece344a368dea6cf84433b0
FritzDavid <fritz.david94@googlemail.com>
2019-10-21 11:37:03 +0200

    Initial commit

---
+++ Assets/golfclub.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class golfclub : MonoBehaviour
+{
+   public GameObject golfclub_object; 
+
+   // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        golfclub_object.transform.position = this.transform.position;
+		golfclub_object.transform.rotation = this.transform.rotation;
+    }
+}
