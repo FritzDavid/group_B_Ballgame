58ef3acb05d5d7d9fcc930827011114d35e8053d
FritzDavid <fritz.david94@googlemail.com>
2019-10-28 12:57:43 +0100

    controller position

--- Assets/golfclub.cs
+++ Assets/golfclub.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class golfclub : MonoBehaviour
 {
-   public GameObject golfclub_object; 
 
-   // Start is called before the first frame update
+    public Vector3 pos;
+   public GameObject golfclub_object;
+   public Quaternion q;
+    // Start is called before the first frame update
     void Start()
     {
+        Debug.Log("ratoation: " + Quaternion.Angle(golfclub_object.transform.rotation, this.transform.rotation));
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        golfclub_object.transform.position = this.transform.position;
+        pos.x = -0.05f;
+        pos.y = 0.07f;
+        pos.z = 0.2f;
+
+        Vector3 positionVektor = new Vector3(this.transform.position.x + pos.x, this.transform.position.y + pos.y, this.transform.position.z + pos.z);
+        Vector3 rotationVector;
+
+       
+
+
+
+        // generate a random euler angle
+        rotationVector.x = this.transform.rotation.x;
+        rotationVector.y = this.transform.rotation.y;
+        rotationVector.z = this.transform.rotation.z;
+
+
+        // convert the euler into a quaternion
+        q = Quaternion.Euler(rotationVector);
+
+   
+
+        golfclub_object.transform.position = positionVektor;
+
 		golfclub_object.transform.rotation = this.transform.rotation;
+        golfclub_object.transform.Rotate(new Vector3(-70.0f, 30.0f, 0.0f)); 
+        
+        
+
+        Debug.Log("rotationVector.x: " + rotationVector.x +  "rotationVector.y: " + rotationVector.y +  "rotationVector.z: " + rotationVector.z);
+
+
     }
 }
