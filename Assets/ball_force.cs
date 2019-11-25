f5f6c291e71b14ce423e62785c536c78ffe6a02b
Nils Federle <feni1011@hs-karlsruhe.de>
2019-11-25 12:41:08 +0100

    Added Velocity from club to ball ; teleport the player camera to resting ball

---
+++ Assets/ball_force.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_force : MonoBehaviour
+{
+    public Vector3 old_position;
+    public Vector3 delta_position;
+    public Vector3 velocity;
+    public float t; //Zeit
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        old_position = this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Time.deltaTime;
+
+        delta_position = this.transform.position - old_position;
+        velocity = delta_position / t;
+
+
+       
+
+        old_position = this.transform.position;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        other.attachedRigidbody.velocity = velocity;
+    }
+}
---
+++ Assets/teleport_player_to_ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleport_player_to_ball : MonoBehaviour
+{
+    public GameObject ball;
+    public Vector3 old_position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        old_position = ball.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.SqrMagnitude(ball.transform.position - old_position) < 0.00000000000000001)
+        {
+            this.transform.position = new Vector3(ball.transform.position.x, this.transform.position.y, ball.transform.position.z) ;
+        }
+        old_position = ball.transform.position;
+    }
+}
