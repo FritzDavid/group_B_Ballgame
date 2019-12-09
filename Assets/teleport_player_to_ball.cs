08e11ad92af3ec8ba69161638c60c0dc02b409fa
Nils Federle <feni1011@hs-karlsruhe.de>
2019-12-09 12:54:45 +0100

    Added Castle, changed colour of ball, mesh collider on Iron, teleport with counter

--- Assets/teleport_player_to_ball.cs
+++ Assets/teleport_player_to_ball.cs
@@ -6,19 +6,31 @@ public class teleport_player_to_ball : MonoBehaviour
 {
     public GameObject ball;
     public Vector3 old_position;
+    public int samePositionCounter;
 
     // Start is called before the first frame update
     void Start()
     {
         old_position = ball.transform.position;
+        samePositionCounter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.SqrMagnitude(ball.transform.position - old_position) < 0.00000000000000001)
+        if (Vector3.SqrMagnitude(ball.transform.position - old_position) < 0.000000000000000001)
         {
-            this.transform.position = new Vector3(ball.transform.position.x, this.transform.position.y, ball.transform.position.z) ;
+            samePositionCounter++;
+
+            if (samePositionCounter >= 100)
+            {
+                this.transform.position = new Vector3(ball.transform.position.x, this.transform.position.y, ball.transform.position.z);
+                samePositionCounter = 0;
+                Debug.Log("teleport");
+            }
+            
+
+            
         }
         old_position = ball.transform.position;
     }
