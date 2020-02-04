0e24bd30fd39b5eea10d6395be179078e29b9f70
Nils Federle <feni1011@ads.hs-karlsruhe.de>
2020-02-04 12:29:39 +0100

    Onboarding Trigger

---
+++ Assets/GameStart.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR; 
+
+public class GameStart : MonoBehaviour
+{
+    public GameObject ball;
+    public GameObject onBoardingBoard;
+    private float ballYPos = 0;
+    private bool gamestarted = false;
+    public SteamVR_Input_Sources LeftInputSource = SteamVR_Input_Sources.LeftHand;
+    public SteamVR_Input_Sources RightInputSource = SteamVR_Input_Sources.RightHand;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballYPos = ball.transform.position.y;
+        ball.transform.position = new Vector3(ball.transform.position.x, -1 , ball.transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gamestarted == false && SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource) == 1)
+        {
+            ball.transform.position = new Vector3(ball.transform.position.x, ballYPos, ball.transform.position.z);
+            onBoardingBoard.SetActive(false);
+            Debug.Log("Right Trigger value:" + SteamVR_Actions._default.Squeeze.GetAxis(RightInputSource).ToString());
+            gamestarted = true;
+        }
+    }
+
+}
